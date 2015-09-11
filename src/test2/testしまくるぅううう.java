21baa4aead8ebf2cbb76015a283ec388b6e4c37d
unknown <x15g018@PC505N030.chiba-fjb.ac.jp>
2015-09-11 11:39:36 +0900

    aaaa

--- src/test2/testしまくるぅううう.java
+++ src/test2/testしまくるぅううう.java
@@ -3,8 +3,9 @@ package test2;
 public class testしまくるぅううう {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println("けっ");
+		System.out.println("ふっ");
+		
 	}
 
 }
